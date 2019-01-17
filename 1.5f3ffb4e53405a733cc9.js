9fadea389b065224cf882e4781910777ad7d6692
Aral Roca Gomez <aral-rg@hotmail.com>
2019-01-17 22:08:45 +0100

    Add build to github pages

---
+++ 1.5f3ffb4e53405a733cc9.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{102:function(n,o){},104:function(n,o){},137:function(n,o){},138:function(n,o){}}]);
+//# sourceMappingURL=1.5f3ffb4e53405a733cc9.js.map
--- README.md
+++
@@ -1,9 +0,0 @@
-# running-stabilizer 🏃‍♀️🏃‍♂️
-
-Stabilizing any YouTube video meanwhile you are running on a jogging machine.
-
-## Tecnologies used:
-
-* Tensorflow.js
-* React
-* Webpack
