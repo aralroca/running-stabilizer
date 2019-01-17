58d121f9f5429ef99a70d778026d50f78aa69441
Aral Roca Gomez <aral-rg@hotmail.com>
2019-01-17 18:22:48 +0100

    Move logic into useCamaraPoses hook

--- src/App.js
+++ src/App.js
@@ -2,102 +2,18 @@ import React, { useState, useCallback, useEffect, useRef } from 'react'
 import { Form, Input } from 'react-form-uncontrolled'
 import Header from './components/Header'
 import Iframe from './components/Iframe'
+import useCamaraPoses from './hooks/useCamaraPoses';
 
 import './assets/styles/app.css'
 
-import('@tensorflow-models/posenet')
-
 const VIDEO_DEFAULT = '_fVBFE2xw7U'
-const MILLISECONDS = 500
-const weight = 0.5
-const imageScaleFactor = 0.5
-const outputStride = 16
-const flipHorizontal = true
-const maxVideoSize = 300
-let timeout
-let video
 
 export default function App(){
-  const [net, setNet] = useState()
-  const videoElement = useRef(null)
   const [videoID, setVideoID] = useState(VIDEO_DEFAULT)
   const onBrowse = useCallback(({ id }) => setVideoID(id), [])
+  const { videoElement, poses } = useCamaraPoses()
 
-  useEffect(() => {
-    async function setupCamera(){
-      videoElement.current.width = maxVideoSize
-      videoElement.current.height = maxVideoSize
-    
-      if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-        const stream = await navigator.mediaDevices.getUserMedia({
-          'audio': false,
-          'video': {
-            facingMode: 'user',
-            width:  maxVideoSize,
-            height: maxVideoSize
-          }
-        })
-        videoElement.current.srcObject = stream
-    
-        return new Promise(resolve => {
-          videoElement.current.onloadedmetadata = () => {
-            resolve(videoElement.current)
-          }
-        })
-      } else {
-        const errorMessage = "This browser does not support video capture, or this device does not have a camera"
-        alert(errorMessage)
-        return Promise.reject(errorMessage)
-      }
-    }
-
-    async function loadVideo(){
-      const video = await setupCamera()
-  
-      video.play()
-    
-      return video
-    }
-
-    async function capture(){
-        if(!videoElement || !net){
-          timeout = setTimeout(capture, MILLISECONDS)
-          return
-        }
-    
-        if(!video && videoElement){
-           video = await loadVideo()
-        }
-    
-        const poses = await net
-          .estimateSinglePose(video, imageScaleFactor, flipHorizontal, outputStride)
-    
-        console.log({ poses })
-    
-        timeout = setTimeout(capture, MILLISECONDS)
-    }
-
-    async function init(){
-        const posenet = await import('@tensorflow-models/posenet')
-        const net = await posenet.load(weight)
-        
-        setNet(net)
-    }
-
-    if(videoElement && net) {
-      timeout = setTimeout(capture, MILLISECONDS)
-    } else {
-      init()
-    }
-    return () => clearTimeout(timeout)
-  }, [videoElement, net])
-
-
-  useEffect(() => {
-    if(videoID){
-      console.log('TODO: Prediction here', videoID)
-    }
-  }, [videoID])
+  console.log({ poses })
 
   return (
     <>
